77185716fe53376b1cc58f9424bc16c1ff13cd03
Bach Tran <batr.schip@gmail.com>
2019-11-09 21:52:25 +1100

    init folds.hs

---
+++ exam/folds.hs
@@ -0,0 +1,15 @@
+-- File: folds.hs
+-- Author: Bach Tran
+-- Revision & playground for fold functions.
+-- 2019, Bach Tran. All rights reserved.
+
+-- import Prelude hiding (foldr, foldl)
+
+veryBigList = [1..1000000]
+
+sum1 :: Num n => [n] -> n
+sum1 [] = 0
+sum1 (x:xs) = x + sum1 xs
+
+sum2 :: Num n => [n] -> n
+sum2 numlist = foldr (+) 0 numlist
