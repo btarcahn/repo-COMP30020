aa4bc45604f7e1cf08bc97b254b8eadbaa720436
Bach Tran <batr.schip@gmail.com>
2019-11-12 14:52:48 +1100

    balanced fold

--- exam/folds.hs
+++ exam/folds.hs
@@ -12,18 +12,35 @@ import Prelude hiding (foldl, foldr)
 {-- |Note: the type similar to the accumulator
 and the Final value is on the LEFT. This explains
 why the binary function is (v -> e -> v).
+This function is tail recursive, and lazy
 -}
 foldl :: (v -> e -> v) -> v -> [e] -> v
 foldl _ base [] = base
 foldl f base (x:xs) =
     let newbase = f base x in
         foldl f newbase xs
-
+{-- |Note: the type similar to the accumulator
+and the Final value is on the RIGHT. This explains
+why the binary function is (e -> v -> v).
+This function is NOT tail recursive.
+-}
 foldr :: (e -> v -> v) -> v -> [e] -> v
 foldr _ base [] = base
 foldr f base (x:xs) =
     let fxs = foldr f base xs in
         f x fxs
+
+foldb :: (e -> e -> e) -> e -> [e] -> e
+foldb _ b [] = b
+foldb _ _ [x] = x
+foldb f b l@(_:_:_) =
+    let
+        len = length l
+        (half1, half2) = splitAt (div len 2) l
+        value1 = foldb f b half1
+        value2 = foldb f b half2
+    in
+        f value1 value2
 
 -- e and v of the same type
 suml :: Num a => [a] -> a
@@ -32,8 +49,23 @@ suml = foldl (+) 0
 productl :: Num a => [a] -> a
 productl = foldl (*) 1
 
+{--
+Cost of (++) is asymmetrical, it
+depends on the LEFT argument.
+e.g. [long_list] ++ [long_list]
+You have to RUN THROUGH the first
+argument of (++) to calculate the
+concatenation.
+-}
+
 concatl :: [[a]] -> [a]
 concatl = foldl (++) []
+{-- |Note: this function performs
+better than foldr, since the RIGHT
+argument of (++) is much less costly.
+-}
+concatr :: [[a]] -> [a]
+concatr = foldr (++) []
 
 sumlengthl :: [[a]] -> Int
 sumlengthl = foldl (flip ((+) . length)) 0
